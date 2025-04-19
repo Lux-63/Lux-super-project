03a4904c948fe450cfb2d007f3aa5fa797552a89
Lux-63 <Lux-63@yandex.ru>
2025-04-19 20:48:14 +0500

    different variants of the logic of the game of life have been adjusted. array and obj

---
+++ src/GameOfLife/OptionWithArray.vue
@@ -0,0 +1,364 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+  fieldWidth: Number,
+  fieldHeight: Number
+})
+
+/*
+проход по соседям идет и в отрицательные числаБ возможно поэтому он не корректно отображается
+
+
+
+Удаление свойств из объектов во вью через стандартный "delete" не будет работать корректно, если объект/массив является реактивным.
+добавить счетчик эпох.
+что будет, если все поле будет в живых клетках.
+*/
+
+let centerX = ref(0);
+let centerY = ref(0);
+const canvasElementRef = ref(null);
+let canvasContext = null;
+let epochCounter = ref(0)
+let timerId = null;
+let allCellX = 0;
+let allCellY = 0;
+let testX = ref(0);
+//let testY = inject(y.value);
+
+let indexY = 0;
+let indexX = 0;
+
+let population = [];
+let deadCells = [];
+let bornCells = [];
+
+onMounted(() => {
+  canvasContext = canvasElementRef.value.getContext("2d");
+  allCellX = props.fieldWidth / 10;
+  allCellY = props.fieldHeight / 10;
+  testX.value = props.fieldWidth / 10;
+
+  for (let x = 0; x <= allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+
+  // ToDo посмотреть другие способы не через циклы. Или оставить один цикл.
+  for (let i = 0; i < allCellY; i++) {
+    indexY = i;
+    population.push([]);
+    for (let i = 0; i < allCellX; i++) {
+      indexX = i;
+      population[indexY].push(0);
+    }
+    console.log(population, indexY, indexX, "параметры из родителя", testX.value);
+  }
+
+  //Пробрасываем параметры от родителя.
+  // testX = props.fieldWidth
+});
+
+// вызвать отдельно популяцию, по которой канвас уже нарисует все. В отдельной функции.
+function test() {
+  //console.log(allCellX, allCellY)
+  canvasContext
+  let addLivingCell = [
+    [6, 6],
+    [7, 6],
+    [8, 6],
+    [4, 8],
+    [8, 10],
+    [8, 11],
+    [8, 12],
+    [9, 12],
+    [10, 12],
+    [10, 11],
+    [10, 10],
+    [9, 10],
+    [3, 3],
+    [4, 4],
+    [5, 4],
+    [5, 3],
+    [5, 2],
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [39, 0],
+  ]
+  indexY = 0;
+  //Добавляем живие клетки в массив. addLivingCell переименовать, должно нач с сущ.
+  for (let key in addLivingCell) {
+    // population[addLivingCell[key][0]].splice(addLivingCell[key][1], 1, 1);
+
+    population[addLivingCell[key][0]][addLivingCell[key][1]] = 1;
+    // console.log(population);
+  }
+  // Рисуем живые клетки из массива. убрать лишнее.
+  for (let i = 0; i < population.length; i++) {
+    indexY = i;
+    population[i].forEach((item, index) => {
+      if (item == 1) {
+        canvasContext.fillRect(index * 10, indexY * 10, 10, 10);
+      }
+    });
+  }
+
+  console.log("проверим параметры", testX.value, allCellX, allCellY);
+}
+
+/**
+ * Следующий шаг эволюции.
+ */
+function nextStep() {
+  for (let y = 0; y < population.length; y++) {
+    for(let x = 0; x < population[y].length; x++) {
+      if (population[y][x] == 1) {
+        checkingNeighbors(y, x);
+      }
+    }
+  }
+  epochCounter.value++;
+  cellEvolution();
+}
+/**
+ * Проверяет сколько соседей есть у живой клетки.
+ * @param x {number}
+ * @param y {number}
+ */
+function checkingNeighbors(y, x) {
+  // let coordinateX = x;
+  // let coordinateY = y;
+  let livingNeighbor = 0;
+  let coordinateStep = -1;
+  
+  const coordinate = [
+    [y - 1, x - 1],
+    [y - 1, x],
+    [y - 1, x + 1],
+    [y, x + 1],
+    [y + 1, x + 1],
+    [y + 1, x],
+    [y + 1, x - 1],
+    [y, x - 1],
+  ];
+  //если выдавать ноль, то он будет проверять отрицательные значения. А этого допускать нельзя!
+  const neighboringCell = [
+    y > 0 && x > 0 ? population[y-1][x-1] : -1,
+    y > 0 ? population[y-1][x] : -1,
+    y > 0 && x < allCellX - 1 ? population[y-1][x+1] : -1,
+    y > 0 && x < allCellX - 1 ? population[y][x+1] : -1,
+    y < allCellY - 1 && x < allCellX ? population[y+1][x+1] : -1,
+    y < allCellY - 1 ? population[y+1][x] : -1,
+    y < allCellY - 1 && x > 0 ? population[y+1][x-1] : -1,
+    x > 0 ? population[y][x-1] : -1,
+  ];
+
+  for (let key in neighboringCell) {
+    coordinateStep++
+    
+    if (neighboringCell[key] == 1) {
+      livingNeighbor++
+    } else if(neighboringCell[key] == 0) {
+
+      //отправляет пустую клетку на проверку рождения.
+      birthCell(coordinate[coordinateStep][0], coordinate[coordinateStep][1]);
+    }
+  }
+  if(livingNeighbor < 2 || livingNeighbor > 3) {
+    deadCells.push([y, x])
+  }
+}
+
+/**
+ * Проверяем родится ли новая клетка.
+ * @param x {number}
+ * @param y {number}
+ */
+function birthCell(y, x) {
+
+  let livingNeighbor = 0;
+  const neighboringCell = [
+    y > 0 && x > 0 ? population[y-1][x-1] : -1,
+    y > 0 ? population[y-1][x] : -1,
+    y > 0 && x < allCellX - 1 ? population[y-1][x+1] : -1,
+    y > 0 && x < allCellX - 1 ? population[y][x+1] : -1,
+    y < allCellY - 1 && x < allCellX ? population[y+1][x+1] : -1,
+    y < allCellY - 1 ? population[y+1][x] : -1,
+    y < allCellY - 1 && x > 0 ? population[y+1][x-1] : -1,
+    x > 0 ? population[y][x-1] : -1,
+  ];
+  // перебор соседей.
+  for (let key in neighboringCell) {
+
+    if(neighboringCell[key] == 1) {
+      livingNeighbor++
+    }
+  }
+
+  if(livingNeighbor == 3) {
+    bornCells.push([y, x]);
+  }
+}
+
+/**
+ * отрисовка следующего поколения.
+ */
+function cellEvolution() {
+  for (let key in deadCells) {
+    canvasContext.clearRect(deadCells[key][1] * 10, deadCells[key][0] * 10, 10, 10);
+   
+    population[deadCells[key][0]].splice(deadCells[key][1], 1, 0)
+  }
+
+  for (let key in bornCells) {
+    population[bornCells[key][0]].splice(bornCells[key][1], 1, 1)
+    canvasContext.fillRect(bornCells[key][1] * 10 , bornCells[key][0] * 10, 10, 10);
+  }
+  bornCells = [];
+  deadCells = [];
+
+  // Восстанавливаем клетки. Цвет меняется потому что разметка становится сверху?
+  for (let x = 0; x < allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+}
+/**
+ * Рисуем живие клетки в поле.
+ * @param x {number}
+ * @param y {number}
+ */
+function positionDiv(x, y) {
+  centerX.value = Math.floor((x - canvasElementRef.value.getBoundingClientRect().x) / 10);
+  centerY.value = Math.floor((y - canvasElementRef.value.getBoundingClientRect().y) / 10);
+  if( centerX.value >= allCellX) {
+    centerX.value--;
+  }
+  if (centerY.value >= allCellY) {
+    centerY.value--;
+  }
+}
+function positionCanvas(x, y) {
+  x = Math.floor((x - canvasElementRef.value.getBoundingClientRect().x) / 10);
+  y = Math.floor((y - canvasElementRef.value.getBoundingClientRect().y) / 10);
+  if( x >= allCellX) {
+    x--;
+  }
+  if (y >= allCellY) {
+    y--;
+  }
+  
+
+  canvasContext.fillRect(x * 10, y * 10, 10, 10);
+  population[y].splice(x, 1, 1)
+}
+
+function clearArea() {
+  canvasContext.clearRect(0, 0, 400, 400);
+  //переписываем массив клеток.
+  allCellX = 400 / 10;
+  allCellY = 400 / 10;
+  population = [];
+  for (let i = 0; i < allCellY; i++) {
+    indexY = i;
+    population.push([]);
+    for (let i = 0; i < allCellX; i++) {
+      indexX = i;
+      population[indexY].push(0);
+      console.log(population)
+    }
+  }
+  epochCounter.value = 0;
+  // Восстанавливаем клетки
+  for (let x = 0; x <= allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+};
+
+function autoStartGame() {
+  timerId = setInterval(() => nextStep(), 0);
+  timerId;
+};
+function stopGame() {
+  clearInterval(timerId);
+  console.log(population);
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <button
+      type="button"
+      @click="nextStep"
+    >
+      шаг
+    </button>
+    <button
+      type="button"
+      @click="autoStartGame"
+    >
+      старт
+    </button>
+    <button 
+      type="button"
+      @click="stopGame"
+    >
+      стоп
+    </button>
+    <button 
+      type="button" 
+      @click="clearArea"
+    >
+      сброс
+    </button>
+    <button
+      type="button"
+      @click="test"
+    >
+      тест
+    </button>
+  </div>
+  <div class="container text-center">
+    <canvas
+      ref="canvasElementRef"
+      class="canvas-size position canvas-style border border-black center"
+      width="400px"
+      height="400px"
+      
+      
+      @mousemove="positionDiv($event.clientX, $event.clientY)"
+      @click="positionCanvas($event.clientX, $event.clientY)"
+    />
+  </div>
+  <div class="container text-center">
+    Текущее поколение: {{ epochCounter }} 
+  </div>
+</template>
+
+<style>
+.canvas-size1 {
+  width: 400px;
+  height: 400px;
+}
+</style>
---
+++ src/GameOfLife/OptionWithObject.vue
@@ -0,0 +1,365 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+/*
+проход по соседям идет и в отрицательные числаБ возможно поэтому он не корректно отображается
+
+
+
+Удаление свойств из объектов во вью через стандартный "delete" не будет работать корректно, если объект/массив является реактивным.
+добавить счетчик эпох.
+что будет, если все поле будет в живых клетках.
+*/
+
+let centerX = ref(0);
+let centerY = ref(0);
+const canvasElementRef = ref(null);
+let canvasContext = null;
+let timerId = null;
+let allCellX = 400;
+let allCellY = 400;
+
+let population = {};
+let deadCells = [];
+let bornCells = {};
+
+onMounted(() => {
+  canvasContext = canvasElementRef.value.getContext("2d");
+  allCellX = allCellX / 10;
+  allCellY = allCellY / 10;
+  console.log(allCellX, allCellY)
+  for (let x = 0; x <= allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+});
+
+/* подумать что бы поуляция была в виде массива. сделать второй вариант отдельно, где популешин будет массивов.
+  [
+    [0, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0]
+  ]
+*/
+
+// вызвать отдельно популяцию, по которой канвас уже нарисует все. В отдельной функции.
+function test() {
+  population = {
+    "60,60": [60, 60],
+    "60,70": [60, 70],
+    "60,80": [60, 80],
+    "80,40": [80, 40],
+    "100,80": [100, 80],
+    "110,80": [110, 80],
+    "120,80": [120, 80],
+    "120,90": [120, 90],
+    "120,100": [120, 100],
+    "110,100": [110, 100],
+    "100,100": [100, 100],
+    "100,90": [100, 90],
+    "30,30": [30, 30],
+    "40,40": [40, 40],
+    "40,50": [40, 50],
+    "30,50": [30, 50],
+    "20,50": [20, 50],
+    "0,0": [0, 0],
+    "10,0": [10, 0],
+    "20,0": [20, 0],
+    "0,390": [0, 390],
+  };
+  for (let key in population) {
+    canvasContext.fillRect(population[key][0], population[key][1], 10, 10);
+  }
+
+  //console.log("популяция", population);
+}
+/**
+ * Следующий шаг эволюции.
+ */
+function nextStep() {
+  //console.log("шаг эволюции");
+  let coordinateX = 0;
+  let coordinateY = 0;
+  for (let key in population) {
+    coordinateX = population[key][0];
+    coordinateY = population[key][1];
+    //console.log("шаг", coordinateX, coordinateY);
+    checkingNeighbors(coordinateX, coordinateY);
+  }
+  cellEvolution();
+}
+/**
+ * Проверяет сколько соседей есть у живой клетки.
+ * @param x {number}
+ * @param y {number}
+ */
+function checkingNeighbors(x, y) {
+  //console.log("приняли", x, y);
+  const neighboringCell = [
+    [x - 10, y - 10],
+    [x, y - 10],
+    [x + 10, y - 10],
+    [x + 10, y],
+    [x + 10, y + 10],
+    [x, y + 10],
+    [x - 10, y + 10],
+    [x - 10, y],
+  ];
+
+  // индекс мертвой клетки что бы проверить, родится ли она
+  let indexPossiblyBornCell = -1;
+  let livingNeighbor = 0;
+
+  const identifyNeighbors = {
+    1: population[`${neighboringCell[0][0]},${neighboringCell[0][1]}`],
+    2: population[`${neighboringCell[1][0]},${neighboringCell[1][1]}`],
+    3: population[`${neighboringCell[2][0]},${neighboringCell[2][1]}`],
+    4: population[`${neighboringCell[3][0]},${neighboringCell[3][1]}`],
+    5: population[`${neighboringCell[4][0]},${neighboringCell[4][1]}`],
+    6: population[`${neighboringCell[5][0]},${neighboringCell[5][1]}`],
+    7: population[`${neighboringCell[6][0]},${neighboringCell[6][1]}`],
+    8: population[`${neighboringCell[7][0]},${neighboringCell[7][1]}`],
+  };
+
+  // перебор соседей.
+  for (let key in identifyNeighbors) {
+    //тут делаем реверс на противоположную сторону. Проверяем отрицательные либо плюсовые значения.
+    // Внутри уже проверяем наличие данных
+    indexPossiblyBornCell += 1; // подумать как переименовать что бы было понятно.
+    if (identifyNeighbors[key] != undefined) {
+      livingNeighbor += 1;
+
+      //console.log("координаты и сосед", livingNeighbor, indexPossiblyBornCell);
+    }
+    if (identifyNeighbors[key] === undefined) {
+      birthCell(neighboringCell[indexPossiblyBornCell][0], neighboringCell[indexPossiblyBornCell][1]);
+
+      // console.log(
+      //   " смотрим что передаст",
+      //   x,
+      //   y,
+      //   neighboringCell[indexPossiblyBornCell][0],
+      //   neighboringCell[indexPossiblyBornCell][1]
+      // );
+    }
+  }
+
+  if (livingNeighbor < 2 || livingNeighbor > 3) {
+    //console.log("клетка умирает", livingNeighbor);
+
+    deadCells.push([x, y]);
+    // console.log(
+    //   "клетка умирает",
+    //   livingNeighbor,
+    //   population,
+    //   deadCells,
+    //   deadCells[0]
+    // );
+  }
+}
+
+/**
+ * Проверяем родится ли новая клетка.
+ * @param x {number}
+ * @param y {number}
+ */
+function birthCell(x, y) {
+  //console.log("сюда кидаем пустую клетку и проверяем соседей", x, y);
+  const neighboringCell = [
+    [x - 10, y - 10],
+    [x, y - 10],
+    [x + 10, y - 10],
+    [x + 10, y],
+    [x + 10, y + 10],
+    [x, y + 10],
+    [x - 10, y + 10],
+    [x - 10, y],
+  ];
+  let livingNeighbor = 0;
+
+  const identifyNeighbors = {
+    1: population[`${neighboringCell[0][0]},${neighboringCell[0][1]}`],
+    2: population[`${neighboringCell[1][0]},${neighboringCell[1][1]}`],
+    3: population[`${neighboringCell[2][0]},${neighboringCell[2][1]}`],
+    4: population[`${neighboringCell[3][0]},${neighboringCell[3][1]}`],
+    5: population[`${neighboringCell[4][0]},${neighboringCell[4][1]}`],
+    6: population[`${neighboringCell[5][0]},${neighboringCell[5][1]}`],
+    7: population[`${neighboringCell[6][0]},${neighboringCell[6][1]}`],
+    8: population[`${neighboringCell[7][0]},${neighboringCell[7][1]}`],
+  };
+
+  // перебор соседей.
+  for (let key in identifyNeighbors) {
+    if (identifyNeighbors[key] != undefined) {
+      livingNeighbor += 1;
+      //console.log("смотрим пустых соседей", livingNeighbor, x, y);
+    }
+  }
+  if (livingNeighbor === 3 && population[`${x},${y}`] == undefined) {
+    if (x >= 0 && (x < 400) & (y >= 0) && y < 400) {
+      bornCells[`${x},${y}`] = [x, y];
+    }
+  }
+  //console.log("рождение", x, y, bornCells, livingNeighbor);
+}
+
+/**
+ * отрисовка следующего поколения.
+ */
+function cellEvolution() {
+  //console.log("Тут смотрим финал");
+  deadCells.forEach((i) => {
+    delete population[`${i[0]},${i[1]}`];
+    canvasContext.clearRect(i[0], i[1], 10, 10);
+    // console.log(
+    //   "удалим мертвых",
+    //   population,
+    //   population[(i[0], i[1])],
+    //   i[0],
+    //   i[1]
+    // );
+  });
+
+  for (let key in bornCells) {
+    population[`${bornCells[key][0]},${bornCells[key][1]}`] = [
+      bornCells[key][0],
+      bornCells[key][1],
+    ];
+    canvasContext.fillRect(bornCells[key][0], bornCells[key][1], 10, 10);
+
+    // console.log(
+    //   "добавим живых",
+    //   bornCells[key],
+    //   bornCells[key][0],
+    //   bornCells[key][1],
+    //   population
+    // );
+  }
+  bornCells = {};
+  deadCells = [];
+
+  // Восстанавливаем клетки. Цвет меняется потому что разметка становится сверху?
+  for (let x = 0; x <= allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+};
+/**
+ * Рисуем живие клетки в поле.
+ * @param x {number}
+ * @param y {number}
+ */
+function positionDiv(x, y) {
+  centerX.value = Math.floor((x - canvasElementRef.value.getBoundingClientRect().x) / 10);
+  centerY.value = Math.floor((y - canvasElementRef.value.getBoundingClientRect().y) / 10);
+  if( centerX.value >= allCellX) {
+    centerX.value --;
+  }
+  if (centerY.value >= allCellY) {
+    centerY.value --;
+  }
+}
+function positionCanvas(x, y) {
+  x =  Math.floor((x - canvasElementRef.value.getBoundingClientRect().x) / 10);
+  y =  Math.floor((y - canvasElementRef.value.getBoundingClientRect().y) / 10);
+  if( x >= allCellX) {
+    x --;
+  }
+  if (y >= allCellY) {
+    y --;
+  }
+
+  canvasContext.fillRect(x * 10, y * 10, 10, 10);
+  population[`${x * 10},${y * 10}`] = [x * 10, y * 10];
+  console.log(population)
+  /*
+  556/10 = 55,6
+  работать как с числами, а не строками.
+  */
+}
+
+function clearArea() {
+  canvasContext.clearRect(0, 0, allCellX * 10, allCellY * 10);
+  population = {};
+
+  // Восстанавливаем клетки
+  for (let x = 0; x <= allCellX * 10; x += 10) {
+    canvasContext.moveTo(x, 0);
+    canvasContext.lineTo(x, allCellX * 10);
+  }
+  for (let y = 0; y < allCellY * 10; y += 10) {
+    canvasContext.moveTo(0, y);
+    canvasContext.lineTo(allCellY * 10, y);
+  }
+  canvasContext.strokeStyle = "silver";
+  canvasContext.stroke();
+}
+function autoStartGame() {
+  timerId = setInterval(() => nextStep(), 0);
+  timerId;
+}
+function stopGame() {
+  clearInterval(timerId);
+  console.log(population);
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <button
+      type="button"
+      @click="nextStep"
+    >
+      шаг
+    </button>
+    <button
+      type="button"
+      @click="autoStartGame"
+    >
+      старт
+    </button>
+    <button 
+      type="button"
+      @click="stopGame"
+    >
+      стоп
+    </button>
+    <button 
+      type="button" 
+      @click="clearArea"
+    >
+      сброс
+    </button>
+    <button
+      type="button"
+      @click="test"
+    >
+      тест
+    </button>
+  </div>
+  <div class="container text-center">
+    <canvas
+      ref="canvasElementRef"
+      class="position canvas-style border border-black center"
+      height="400px"
+      width="400px"
+      @mousemove="positionDiv($event.clientX, $event.clientY)"
+      @click="positionCanvas($event.clientX, $event.clientY)"
+    />
+  </div>
+  <div class="container text-center">
+    {{ centerX }} {{ centerY }} obj
+  </div>
+</template>
