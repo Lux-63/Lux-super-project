8c49d6e479b07c8166733de078dab8f66abac170
Lux-63 <Lux-63@yandex.ru>
2026-02-25 21:56:17 +0500

    working with display

--- src/page_dogs/PageDogs.vue
+++ src/page_dogs/PageDogs.vue
@@ -1,18 +1,20 @@
 <script setup>
 import { ref, onMounted  } from 'vue';
-let srcOne = ref("https://images.dog.ceo/breeds/shiba/shiba-17.jpg");
-let srcTwo = ref("https://images.dog.ceo/breeds/terrier-scottish/n02097298_2583.jpg");
-let srcThree = ref("https://images.dog.ceo/breeds/sharpei/noel.jpg")
+let srcOne = ref("one");
+let srcTwo = ref("dva");
+let srcThree = ref("tri")
 let srcDogLink = "";
 const passedDogsLinks = [];
 const nextDogsLinks = [];
+// let stepVariable = [srcOne.value, srcTwo.value, srcThree.value];
 
 onMounted(() => {
+  addDogLink();
   console.log();
 })
 
 /**
- * вытягиваем ссылку на собак по API.
+ * Вытягиваем ссылку на собак по API.
  */
 function getDogLink() {
   console.log("getDogLink", typeof srcDogLink)
@@ -21,21 +23,41 @@ function getDogLink() {
   resolve.then(result => nextDogsLinks.push(result.message));
   console.log(nextDogsLinks, "getDogLink")
 };
-
+/**
+ * Закидываем линки в массив.
+ */
 function addDogLink() {
+  let stepVariable = [srcOne, srcTwo, srcThree];
   for(let i = nextDogsLinks.length; i < 3; i++) {
     getDogLink();
-    
     console.log("добавится ли ссылки на собак", srcDogLink, nextDogsLinks);
 }
+};
 
+/**
+ * Отображение линков пользователю. 
+ */
+function srcDogView() {
+  let stepVariable = [srcOne, srcTwo, srcThree];
+  // stepVariable[0] = nextDogsLinks[0]
+  for(let i = 0; i < 3; i++) {
+    stepVariable[i].value = nextDogsLinks[i];
+    console.log("smotrim", stepVariable[i], stepVariable)
+  };
+};
+function test() {
+  console.log("nachalo", srcOne.value, srcTwo.value, srcThree.value)
+  let stepVariable = [srcOne.value, srcTwo.value, srcThree.value]
+  // srcDogView()
+  console.log("smena link", stepVariable, srcOne.value, srcTwo.value, srcThree.value, nextDogsLinks)
 };
 </script>
 
 <template>
   <div>
-    <button @click="addDogLink">test</button>
-
+    <button @click="addDogLink">addDogLink</button>
+    <button @click="test">test</button>
+    <button @click="srcDogView">srcDogView</button>
 
 
   <div id="carouselExampleAutoplaying" class="carousel slide " data-bs-ride="carousel">
