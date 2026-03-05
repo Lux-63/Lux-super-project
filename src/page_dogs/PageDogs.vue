525c13b61ac9326648e5f3360a82ded0f531f409
Lux-63 <Lux-63@yandex.ru>
2026-03-05 17:55:22 +0500

    changing images

--- src/page_dogs/PageDogs.vue
+++ src/page_dogs/PageDogs.vue
@@ -1,31 +1,47 @@
 <script setup>
 import { reactive, onMounted } from "vue";
-
-const nextDogsLinks = reactive([]);
-
+let currentsDogsLinks = reactive([]);
 onMounted(() => {
+  console.log("loading carusel", document.querySelectorAll(".carousel-item").length)
   loadImageApi();
 });
+
 
 /**
  * Синхронный вариант выгрузки данных из API и загрузки карусели.
  * В данном случае, карусель будет загружаться после получения первого изображения из API.
  */
-function loadCaruselData() {
-  let imageElement = null;
-  for (let i = 0; i < 3; i++) {
-    imageElement = fetch("https://dog.ceo/api/breeds/image/random")
-      .then((result) => {
-        console.log("3");
-        return result.json();
-      })
-      .then((result) => nextDogsLinks.push(result.message))
-      .then(() => console.log("4"));
-  }
-  imageElement.then(
-    () => (document.querySelector(".carousel-item").className += " active"),
-  );
-}
+// function loadCaruselData() {
+//   let imageElement = null;
+//   for (let i = 0; i < 3; i++) {
+//     imageElement = fetch("https://dog.ceo/api/breeds/image/random")
+//       .then((result) => {
+//         console.log("3");
+//         return result.json();
+//       })
+//       .then((result) => currentsDogsLinks.push(result.message))
+//       .then(() => console.log("4"));
+//   }
+//   imageElement.then(
+//     () => (document.querySelector(".carousel-item").className += " active"),
+//   );
+// }
+
+/**
+ * Асинхронный вариант выгрузки данных из API и загрузки карусели.
+ * В данном случае, карусель будет загружаться после получения всех данных из API.
+ */
+// async function loadImageApi() {
+//   let imageElement = null;
+//   for (let i = 0; i < 3; i++) {
+//     imageElement = await fetch("https://dog.ceo/api/breeds/image/random");
+//     let result = await imageElement.json();
+//     currentsDogsLinks.push(result.message);
+//   }
+//   document.querySelector(".carousel-item").className += " active";
+//   console.log("load dogs", currentsDogsLinks)
+// }
+
 
 /**
  * Асинхронный вариант выгрузки данных из API и загрузки карусели.
@@ -36,29 +52,39 @@ async function loadImageApi() {
   for (let i = 0; i < 3; i++) {
     imageElement = await fetch("https://dog.ceo/api/breeds/image/random");
     let result = await imageElement.json();
-    nextDogsLinks.push(result.message);
+    currentsDogsLinks.push(result.message);
   }
-  document.querySelector(".carousel-item").className += " active";
+  if(document.querySelectorAll(".carousel-item.active").length == 0) {
+    document.querySelector(".carousel-item").className += " active";
+    console.log("load dogs", currentsDogsLinks)
+  }
+
+  // document.querySelector(".carousel-item").className += " active";
+  // console.log("load dogs", currentsDogsLinks)
 }
 
-function test() {}
+
+function displayRandomSrc(){
+  for(let i = currentsDogsLinks.length; i > 0; i--) {
+    currentsDogsLinks.pop()
+  }
+  console.log("typeof currentsDogsLink:", currentsDogsLinks, document.querySelectorAll(".carousel-item"));
+  loadImageApi()
+}
 </script>
 
 <template>
   <div>
-    <button @click="test">test</button>
-
-    <div v-for="linkDog in nextDogsLinks">
-      {{ linkDog }}
-    </div>
+    <button @click="displayRandomSrc">случайные изображения</button>
 
     <div
       id="carouselExampleAutoplaying"
       class="carousel carousel-dark slide"
       data-bs-ride="carousel"
+      
     >
       <div class="carousel-inner">
-        <div v-for="linkDog in nextDogsLinks" class="carousel-item">
+        <div v-for="linkDog in currentsDogsLinks" class="carousel-item" >
           <img :src="linkDog" class="d-block w-70" alt="..." />
         </div>
       </div>
